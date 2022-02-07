06183aa4a503400d9f3e028adeb452dfa09bde4b
Vic van Gool <vic@cloud66.com>
2022-02-07 15:00:43 +0000

    sleep 30 secs healthcheck

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+    def health_check
+        sleep(30)
+        # make sure DB is connectable
+        Post.count
+        head :ok
+    end    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 	root to: 'posts#index'
 	get '/cached', to: 'posts#index'
 
+	get 'health_check' => 'application#health_check'
+
 	resources :posts
 	# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
