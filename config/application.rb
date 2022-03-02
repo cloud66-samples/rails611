16b1e5382ea76ffea6a9ef1d0c43c4c3ac4a493d
Vic van Gool <lvangool@users.noreply.github.com>
2022-03-02 11:09:02 +0000

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,9 @@ module Rails61
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    
+    config.active_record.schema_format = :sql
+    
     config.hosts.clear
   end
 end
